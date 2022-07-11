9dc42bc088193cc66c570307bcc385db2cfbf2f5
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-07-11 15:24:52 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title></title>
+        <meta name='description' content='' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>Welcome!</h1>
+    </div>
+  )
+}
