27234e6e36fc83acbf07b7cd06b8ffd4df486110
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-07-11 15:41:27 +0200

    Add page title

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title></title>
-        <meta name='description' content='' />
+        <title>Digital Downloads</title>
+        <meta name='description' content='Digital Downloads' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <h1>Welcome!</h1>
     </div>
-  )
+  );
 }
