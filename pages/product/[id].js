380f020a68c9fa8a6c7db82abcbc21625231927c
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-07-16 14:14:29 +0200

    Check if user already purchased a product

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,25 +1,36 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
+import { useSession, getSession } from 'next-auth/react';
 
 import prisma from 'lib/prisma';
-import { getProduct } from 'lib/data';
+import { getProduct, alreadyPurchased } from 'lib/data';
 
 import Heading from 'components/Heading';
 import Spinner from 'components/Spinner';
 
 export const getServerSideProps = async context => {
+  const session = await getSession(context);
+
   let product = await getProduct(context.params.id, prisma);
   product = JSON.parse(JSON.stringify(product));
+
+  let purchased = null;
+  if (session) {
+    purchased = await alreadyPurchased(
+      { author: session.user.id, product: context.params.id },
+      prisma
+    );
+  }
 
   return {
     props: {
       product,
+      purchased,
     },
   };
 };
 
-const Product = ({ product }) => {
+const Product = ({ product, purchased }) => {
   const { data: session, status } = useSession();
 
   const loading = status === 'loading';
@@ -61,56 +72,64 @@ const Product = ({ product }) => {
               {!session && <p>Login first</p>}
               {session && (
                 <>
-                  {session.user.id !== product.author.id ? (
-                    <button
-                      className='text-sm border p-2 font-bold uppercase'
-                      onClick={async () => {
-                        if (product.free) {
-                          await fetch('/api/download', {
-                            method: 'POST',
-                            headers: {
-                              'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                              product_id: product.id,
-                            }),
-                          });
-
-                          router.push('/dashboard');
-                        } else {
-                          const res = await fetch('/api/stripe/session', {
-                            method: 'POST',
-                            headers: {
-                              'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                              amount: product.price,
-                              title: product.title,
-                              product_id: product.id,
-                            }),
-                          });
-
-                          const data = await res.json();
-
-                          if (data.status === 'error') {
-                            alert(data.message);
-                            return;
-                          }
-
-                          const sessionId = data.sessionId;
-                          const stripePublicKey = data.stripePublicKey;
-
-                          const stripe = Stripe(stripePublicKey);
-                          const { error } = await stripe.redirectToCheckout({
-                            sessionId,
-                          });
-                        }
-                      }}
-                    >
-                      {product.free ? 'DOWNLOAD' : 'PURCHASE'}
-                    </button>
+                  {purchased ? (
+                    'Already purchased'
                   ) : (
-                    'Your product'
+                    <>
+                      {session.user.id !== product.author.id ? (
+                        <button
+                          className='text-sm border p-2 font-bold uppercase'
+                          onClick={async () => {
+                            if (product.free) {
+                              await fetch('/api/download', {
+                                method: 'POST',
+                                headers: {
+                                  'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify({
+                                  product_id: product.id,
+                                }),
+                              });
+
+                              router.push('/dashboard');
+                            } else {
+                              const res = await fetch('/api/stripe/session', {
+                                method: 'POST',
+                                headers: {
+                                  'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify({
+                                  amount: product.price,
+                                  title: product.title,
+                                  product_id: product.id,
+                                }),
+                              });
+
+                              const data = await res.json();
+
+                              if (data.status === 'error') {
+                                alert(data.message);
+                                return;
+                              }
+
+                              const sessionId = data.sessionId;
+                              const stripePublicKey = data.stripePublicKey;
+
+                              const stripe = Stripe(stripePublicKey);
+                              const { error } = await stripe.redirectToCheckout(
+                                {
+                                  sessionId,
+                                }
+                              );
+                            }
+                          }}
+                        >
+                          {product.free ? 'DOWNLOAD' : 'PURCHASE'}
+                        </button>
+                      ) : (
+                        'Your product'
+                      )}
+                    </>
                   )}
                 </>
               )}
